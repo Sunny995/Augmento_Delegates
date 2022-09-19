7e2a2103ceea1a27615263626418f5f0cbae2303
Sunny995 <51695320+Sunny995@users.noreply.github.com>
2022-09-19 21:03:17 +0530

    Add files via upload

---
+++ InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public static class InputHandler
+    {
+        public static string OnWord(string word)
+        {
+            Console.WriteLine("OnWord Called");
+            return "OnWord";
+        }
+
+        public static string OnNumber(string num)
+        {
+            Console.WriteLine("OnNumber Called");
+            return "OnNumber";
+        }
+
+        public static string OnJunk(string junk)
+        {
+            Console.WriteLine("OnJunk Called");
+            return "OnJunk";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Delegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleReder c = new ConsoleReder();
+            c.Checkinput();
+        }
+    }
+
+    public class ConsoleReder
+    {
+        public delegate string NewDelegate(string word);
+        NewDelegate d1 = new NewDelegate(InputHandler.OnWord);
+        NewDelegate d2 = new NewDelegate(InputHandler.OnNumber);
+        NewDelegate d3 = new NewDelegate(InputHandler.OnJunk);
+
+        public  string Checkinput()
+        {
+            var result= Run(d1,d2,d3);
+            return result;
+        }
+        public  string Run(NewDelegate d1, NewDelegate d2, NewDelegate d3) 
+        {
+            var input = GetNextName();
+
+            if (Regex.IsMatch(input, @"^[a-zA-Z]+$"))
+            {
+                var result = d1("OnWord");
+                return result;
+            }
+            else if (Regex.IsMatch(input, @"^-?[0-9][0-9,\.]+$"))
+            {
+                var result = d2("OnNumber");
+                return result;
+            }
+            else 
+            {
+                var result = d3("OnJunk");
+                return result;
+            }
+
+        }
+
+        public virtual string GetNextName()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+    public class InputReader : ConsoleReder
+    {
+       
+        private string[] inputs = new string[] { "Hello"};
+        private int index = 0;
+        public override string GetNextName()
+        {
+            return inputs[index++];
+        }
+    }
+}
